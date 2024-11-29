2b4d246611541fdb7d06acea592d0eefe3c1951a
BorisVer <borisversonnen@gmail.com>
2024-11-29 12:15:57 +0200

    Lisätty sql

--- src/db_helper.py
+++ src/db_helper.py
@@ -39,7 +39,16 @@ def setup_db():
         "  author VARCHAR(100) NOT NULL, "
         "  title VARCHAR(100) NOT NULL, "
         "  publisher VARCHAR(100) NOT NULL, "
-        "  year INTEGER NOT NULL"
+        "  year INTEGER NOT NULL,  "
+    
+        "  volume VARCHAR(100), "
+        "  series VARCHAR(100), "
+        "  address VARCHAR(100), "
+        "  edition VARCHAR(100), "
+        "  month VARCHAR(100), "
+        "  note TEXT, "
+        "  key VARCHAR(100), "
+        "  url VARCHAR(100)"
         ")"
     )
 
